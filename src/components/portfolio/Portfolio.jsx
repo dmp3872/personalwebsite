807c15cd8d122cfa0bd8ddaef5fc87afd01ab7da
dmp3872 <dmp3872@g.rit.edu>
2022-02-26 19:01:39 -0500

    portfolio error

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -64,7 +64,7 @@ export default function Portfolio() {
         <h1>Portfolio</h1>
         <ul>
           {list.map((item)=>(
-            <PortfolioList title= {item.title} active= {selected == item.id} setSelected = {setSelected}
+            <PortfolioList title= {item.title} active= {selected === item.id} setSelected = {setSelected}
             id = {item.id}
             />
           ))}
@@ -75,7 +75,8 @@ export default function Portfolio() {
             
           <div className="item">
             <img 
-            src= {d.img} />
+            src= {d.img} 
+            alt= ""/>
             <h3>{d.title}</h3>
       
           </div>
