9f3994984d2b0989ae8ddfb908bc8f6312bdce1a
dmp3872 <dmp3872@g.rit.edu>
2022-06-16 16:22:53 -0700

    changed intro pfp

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -17,7 +17,7 @@ export default function Intro() {
     <div className="intro" id= "intro">
         <div className="left">
           <div className="imgContainer">
-              <img src="assests/pfp1.png" alt=""/>
+              <img src="assests/pfp2.png" alt=""/>
              </div>
         </div>
         <div className="right">
